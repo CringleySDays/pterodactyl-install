94f4d30bc1ad366dc59f368ca06d3d9c6d71f9ca
Shaughn De Sousa <cringleysdays@outlook.com>
2025-03-01 23:42:35 +0000

    Update panel-install.sh

--- panel-install.sh
+++ panel-install.sh
@@ -15,45 +15,54 @@ websiteDomain=""
 
 # --------------------------------------------
 
-(sudo grep -v -F "${websiteIP} ${websiteDomain}" /etc/hosts; echo "${websiteIP} ${websiteDomain}") | sudo tee /etc/hosts
-
-sudo apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
-
-sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-
-sudo curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-
-sudo curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash
-sudo apt update
-
-sudo apt -y install php8.3 php8.3-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
-
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-
-# --------------------------------------------
-
-sudo mkdir -p /var/www/pterodactyl
-cd /var/www/pterodactyl
-
-sudo curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
-sudo tar -xzvf panel.tar.gz
-sudo chmod -R 755 storage/* bootstrap/cache/
-
-# --------------------------------------------
-
-echo "" | sudo mariadb -u root -p <<EOF
-USE mysql;
-CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY '${mySQLPassword}';
-CREATE DATABASE panel;
-GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;
-EXIT;
+redis-cli <<EOF
+CONFIG SET requirepass ${mySQLPassword}
+AUTH ${mySQLPassword}
+exit
 EOF
 
 # --------------------------------------------
 
-sudo cp .env.example .env
-sudo COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader
-sudo php artisan key:generate --force
+{
+    echo "${emailAddress}"
+    echo "${websiteDomain}"
+    echo "Europe/London"
+    echo "redis"
+    echo "redis"
+    echo "redis"
+    echo "yes"
+    echo "no"
+    echo "127.0.0.1" # Redis Host
+    echo "${mySQLPassword}" # Redis Password
+    echo "6379"
+} | sudo php artisan p:environment:setup
+
+{
+    echo "127.0.0.1" # Redis Host
+    echo "3306"
+    echo "panel"
+    echo "pterodactyl"
+    echo "${mySQLPassword}" # Redis Password
+} | sudo php artisan p:environment:database
+
+
+{
+    echo "sendmail"
+    echo "${emailAddress}"
+    echo ""
+} | sudo php artisan p:environment:mail
 
 # --------------------------------------------
+
+sudo php artisan migrate --seed --force
+
+{
+    echo "yes"
+    echo "${emailAddress}"
+    echo "${panelUsername}"
+    echo "${panelFirstName}"
+    echo "${panelLastName}"
+    echo "${panelPassword}" # Panel Password
+} | sudo php artisan p:user:make
+
+sudo chown -R www-data:www-data /var/www/pterodactyl/*
